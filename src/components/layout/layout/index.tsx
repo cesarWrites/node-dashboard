9fc486d6f66f395ec8f3bddf5b95402014b9aa0d
cesarWrites <cesarwrites24@gmail.com>
2023-09-18 13:26:10 +0300

    modify app layout

--- src/components/layout/layout/index.tsx
+++ src/components/layout/layout/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { LayoutProps } from "@refinedev/core";
 import Box from "@mui/material/Box";
 
-import { Sider as DefaultSider } from "../sider";
-import { Header as DefaultHeader } from "../header";
+import { Sider as DefaultSider } from "../layout-/sider";
+import { Header as DefaultHeader } from "../layout-/header";
 
 export const Layout: React.FC<LayoutProps> = ({
     Sider,
